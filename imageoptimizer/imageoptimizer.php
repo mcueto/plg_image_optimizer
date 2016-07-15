821cfbb44d8be990727300eb7005702fbfbf9daf
mcueto <cueto@live.cl>
2016-07-15 14:28:48 -0400

    Added: image links saved to database

--- imageoptimizer/imageoptimizer.php
+++ imageoptimizer/imageoptimizer.php
@@ -20,15 +20,40 @@ class plgContentImageoptimizer extends JPlugin
   function _optimizeImage($image){
     $imageList = array();
 
-    /*TODO: optimize and create every size of image*/
-    array_push($imageList,["min","$image"]);
+    /*TODO: optimize and create every size of image, determined by plugin's configuration*/
+    array_push($imageList,["min"=>"$image"]);
+    array_push($imageList,["200"=>"$image"]);
+    array_push($imageList,["400"=>"$image"]);
+    array_push($imageList,["600"=>"$image"]);
 
     return $imageList;
 
   }
 
-  function _createImageConfig($link){
-    return true;
+  function _createImageConfig($image, $imageList){
+    $imageConfig = json_encode(["qualities" => $imageList], JSON_UNESCAPED_SLASHES);
+
+    $db = JFactory::getDbo();
+    // Retrieve the shout
+    $query = $db->getQuery(true);
+
+    // Insert columns.
+    $columns = array('link', 'config');
+
+    // Insert values.
+    $values = array($db->quote($image), $db->quote($imageConfig));
+
+    // Prepare the insert query.
+    $query
+    ->insert($db->quoteName('#__imageoptimizer'))
+    ->columns($db->quoteName($columns))
+    ->values(implode(',', $values));
+
+    // Set the query using our newly populated query object and execute it.
+    $db->setQuery($query);
+    $db->execute();
+
+    return $imageConfig;
   }
 
   function _checkImageConfig($link){
@@ -58,10 +83,9 @@ class plgContentImageoptimizer extends JPlugin
   TODO: validate if a image configuration exists or not
   */
   function _replaceImage(&$image, $config, $quality = 'min'){
-    $config = json_decode($config);
+    $config = json_decode($config, JSON_UNESCAPED_SLASHES);
     foreach ($config->{"qualities"} as $eq) {
       $image = $eq->{$quality};
-      // echo($image);
     }
     return true;
   }
@@ -73,21 +97,25 @@ class plgContentImageoptimizer extends JPlugin
   {
     $images = json_decode($article->images);
     foreach ($images as $key => &$image) {
-      $config = $this->_checkImageConfig($image);
-      if($config){
-        $this->_replaceImage($image, $config, "min");
-      }else{
-        $imageList  = $this->_optimizeImage($image);
-        // $config     = $this->_createImageConfig($imageList);
-        // $this->_replaceImage($image, $config, "min");
+      if(!empty($image)){
+        $config = $this->_checkImageConfig($image);
+        if($config){
+          $this->_replaceImage($image, $config, "min");
+        }else{
+          $imageList  = $this->_optimizeImage($image);
+          $config     = $this->_createImageConfig($image, $imageList);
+          $this->_replaceImage($image, $config, "min");
+        }
       }
+
     }
     $article->images  = json_encode($images);
 
     /*
     TODO:
-    0-if doesnt exists, create config, optimize image
+    0-optimize image(use image_multi_resizer)
     1-add settings to plugin to compress in different sizes(based on json config field)
+    2-add settings to plugin to replaze image in determined quality
     */
 
     return true;
